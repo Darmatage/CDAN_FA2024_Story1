bd322fe795ba3a09e30bebf7a4cd0d21733a114f
ncbourget0 <ncbourget@massart.edu>
2024-10-02 12:42:18 -0400

    Made the pause menu
    Still needs to add to most of the scenes

--- MassArt2024_Story1/Assets/Scripts/GameHandler.cs
+++ MassArt2024_Story1/Assets/Scripts/GameHandler.cs
@@ -2,47 +2,95 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
-public class GameHandler : MonoBehaviour{
+public class GameHandler : MonoBehaviour
+{
 
-        public static int playerStat1;
-        // public GameObject textGameObject;
+    public static int playerStat1;
 
-        // void Start () { UpdateScore (); }
+    public static bool GameisPaused = false;
+    public GameObject pauseMenuUI;
+    public AudioMixer mixer;
+    public static float volumeLevel = 1.0f;
+    private Slider sliderVolumeCtrl;
 
-        void Update(){         //delete this quit functionality when a Pause Menu is added!
-                if (Input.GetKey("escape")){
-                        Application.Quit();
-                }
-
-                // Stat tester:
-                //if (Input.GetKey("p")){
-                //       Debug.Log("Player Stat = " + playerStat1);
-                //}
+    void Awake()
+    {
+        SetLevel(volumeLevel);
+        GameObject sliderTemp = GameObject.FindWithTag("PauseMenuSlider");
+        if (sliderTemp != null)
+        {
+            sliderVolumeCtrl = sliderTemp.GetComponent<Slider>();
+            sliderVolumeCtrl.value = volumeLevel;
         }
+    }
 
-        // void UpdateScore () {
-        //        Text scoreTemp = textGameObject.GetComponent<Text>();
-        //        scoreTemp.text = "Score: " + score; }
+    void Start()
+    {
+        pauseMenuUI.SetActive(false);
+        GameisPaused = false;
+    }
 
-        public void StartGame(){
-                SceneManager.LoadScene("Scene1");
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameisPaused) { Resume(); }
+            else { Pause(); }
         }
+        // Stat tester:
+        //if (Input.GetKey("p")){
+        //       Debug.Log("Player Stat = " + playerStat1);
+        //}
+    }
 
-        public void OpenCredits(){
-                SceneManager.LoadScene("Credits");
-        }
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameisPaused = true;
+    }
 
-        public void RestartGame(){
-                SceneManager.LoadScene("MainMenu");
-        }
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameisPaused = false;
+    }
 
-        public void QuitGame(){
-                #if UNITY_EDITOR
-                UnityEditor.EditorApplication.isPlaying = false;
-                #else
+    public void SetLevel(float sliderValue)
+    {
+        mixer.SetFloat("MusicVolume", Mathf.Log10(sliderValue) * 20);
+        volumeLevel = sliderValue;
+    }
+
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Scene1");
+    }
+
+    public void OpenCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+        // Please also reset all static variables here, for new games!
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
                 Application.Quit();
-                #endif
-        }
+#endif
+    }
 }
